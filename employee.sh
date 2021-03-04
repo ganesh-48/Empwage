07754c281c90a4c3fe06a441e5c180d1e80ba967
ganesh-48 <ganesh.rananavare98@gmail.com>
2021-03-04 19:28:21 +0530

    Added Solving useing case Statement

--- employee.sh
+++ employee.sh
@@ -38,3 +38,20 @@ else
 fi
 
 echo salary=$(( $empHrs * $empRatePerHrs ))
+
+
+
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+empCheck=$((RANDOM%3));
+
+case $empCheck in
+	$isFullTime)
+		empHrs=8;;
+	$isPartTime)
+		empHrs=4;;
+	*)
+		empHrs=0;;
+esac
+echo salary=$(( $empHrs * $empRatePerHrs ))
