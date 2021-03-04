dcd5c59ed7fee36be88c8af507a4ee7de137d6ca
ganesh-48 <ganesh.rananavare98@gmail.com>
2021-03-04 18:40:10 +0530

    Added Employee is Present or Absent

--- employee.sh
+++ employee.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 
 echo "welcome employee wage problem"
+
+isPresent=1;
+randomCheck=$((RANDOM%2));
+
+if [ $isPresent -eq $randomCheck ]
+then
+	echo " Employee is Present"
+else
+	echo "Employee is Absent"
+fi
